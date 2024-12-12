c2bc25f1305dc71263759ef7b79e9b5f5a5dd4bf
Chanran kim <126753671+isliese@users.noreply.github.com>
2024-12-12 14:40:01 -0800

    Update dynamicprogramming.py

--- dynamicprogramming.py
+++ dynamicprogramming.py
@@ -20,14 +20,37 @@ def validate_field(field):
     return True
 
 
-def soccer_dynamic(field):
+# Finds valid paths from (0,0) to (i,j) in the grid using a dynamic programming
+# The number of paths to (i,j) is the sum of paths from (i-1,j) and (i,j-1), provided those cells are valid
+# Takes a list of lists representing the field grid as a parameter
+# Returns the total number of valid paths
+def soccer_dyn_prog(field):
     # Find number of rows in the grid
     r = len(field)
     # Find number of columns in the grid
     c = len(field[0])
-    # ...
+    # Corner case: if the start or end cell is blocked
+    if field[0][0] == "X" or field[r-1][c-1] == "X":
+        return 0
+    
+    # Initialize the DP table
+    A = [[0] * c for _ in range(r)]
+    # Base case: start position
+    A[0][0] = 1  
 
+    # General cases:
+    for i in range(r):
+        for j in range(c):
+            if field[i][j] == "X":
+                A[i][j] = 0  # Blocked cells
+            else:
+                if i > 0:  # Add paths from above
+                    A[i][j] += A[i-1][j]
+                if j > 0:  # Add paths from left
+                    A[i][j] += A[i][j-1]
 
+    return A[r-1][c-1]
+    
 
 # 3x3 grid
 # represented by a list of lists
@@ -44,3 +67,20 @@ field = [
 ['.', '.', 'X', '.', '.', '.', '.', '.', 'X'],
 ['.', '.', '.', '.', '.', '.', '.', '.', '.'] 
 ]
+
+
+# Check if the input grid of the field is valid
+# If it is invalid print an error message
+if not validate_field(field):
+    print("Invalid field.")
+
+else:
+    # If the grid  is valid find all valid paths using an exhaustive search
+    valid_paths_count = soccer_dyn_prog(field)
+    # Print message when no valid paths exist
+    if valid_paths_count == 0:
+        print("No valid paths exist.")
+    # Otherwise print number of valid paths 
+    else:
+        print(f"Total valid paths: {valid_paths_count}")
+        
